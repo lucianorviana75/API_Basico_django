893db49bd8387f72b06d9c3e9b9aef39bdca50c3
LUCIANO RIBEIRO VIANA <lribeiroviana89@gmail.com>
2026-03-05 06:44:33 -0300

    primeiro commit

---
+++ usuarios/views.py
@@ -0,0 +1,13 @@
+#from django.shortcuts import render
+
+# Create your views here.
+
+from rest_framework import viewsets
+from.models import Usuario
+from.serializers import UsuarioSerializer
+
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
+
